f509ef5c4d41213b919c54126cf26111fa9248d6
dhanush3727 <dhanushs4827@gmail.com>
2024-08-27 12:23:18 +0530

    Footer page update

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,4 +1,5 @@
 import React from "react";
+import "./Footer.css";
 import { FaGithub } from "react-icons/fa";
 import { FaLinkedin } from "react-icons/fa";
 import { FaSquareInstagram } from "react-icons/fa6";
@@ -7,37 +8,37 @@ import { IoMdMail } from "react-icons/io";
 const Footer = () => {
   return (
     <div className="bg-[#0b0416]">
-      <div className="footer p-4 flex justify-between items-center">
+      <div className="footer p-4 flex justify-between items-center gap-[20px]">
         <div>
           <p className="text-[#fff] text-center">
             Developed & Designed by <br />
             S. Dhanush
           </p>
         </div>
-        <div>
-          <p className="text-[#fff]">Copyright &copy; 2024 SD</p>
-        </div>
         <div className="flex gap-[10px]">
           <div>
             <a href="https://github.com/dhanush3727" target="_blank">
-              <FaGithub color="#fff" size={20} />
+              <FaGithub color="#fff" size={25} />
             </a>
           </div>
           <div>
             <a href="https://www.linkedin.com/in/dhanush15/" target="_blank">
-              <FaLinkedin color="#fff" size={20} />
+              <FaLinkedin color="#fff" size={25} />
             </a>
           </div>
           <div>
             <a href="https://www.instagram.com/s.dhanush_15" target="_blank">
-              <FaSquareInstagram color="#fff" size={20} />
+              <FaSquareInstagram color="#fff" size={25} />
             </a>
           </div>
           <div>
             <a href="mailto:dhanushs4827@gmail.com" target="_blank">
-              <IoMdMail color="#fff" size={20} />
+              <IoMdMail color="#fff" size={25} />
             </a>
           </div>
+        </div>
+        <div>
+          <p className="text-[#fff]">Copyright &copy; 2024 SD</p>
         </div>
       </div>
     </div>
